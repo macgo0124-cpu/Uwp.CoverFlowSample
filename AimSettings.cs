6bc0a514f6b61f2d9a13ccb42625d2b7a67a5dc9
macgo0124-cpu <macgo0124@gmail.com>
2026-05-30 06:27:05 -0700

    Crear AimSettings.cs

---
+++ AimSettings.cs
@@ -0,0 +1,7 @@
+public static class AimSettings
+{
+    public const float CloseRangeAssist = 0.60f;
+    public const float MediumRangeAssist = 0.40f;
+    public const float LongRangeAssist = 0.25f;
+    public const float TrackingStrength = 0.40f;
+}

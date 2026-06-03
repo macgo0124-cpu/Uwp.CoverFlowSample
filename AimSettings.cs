2e06b6e84d98b7dc39c505e25ed21220f6f274b7
macgo0124-cpu <macgo0124@gmail.com>
2026-06-02 21:46:29 -0700

    Actualizar AimSettings.cs

--- AimSettings.cs
+++ AimSettings.cs
@@ -1,9 +1,10 @@
 public static class AimSettings
 {
-public const float CloseRangeAssist = 1.00f;
-public const float MediumRangeAssist = 0.95f;
-public const float LongRangeAssist = 0.85f;
-public const float TrackingStrength = 1.00f;
+    public const float CloseRangeAssist = 1.00f;
+    public const float MediumRangeAssist = 0.95f;
+    public const float LongRangeAssist = 0.85f;
+    public const float TrackingStrength = 1.00f;
+    
 public class Weapon
 {
     public string Name = "Rifle";

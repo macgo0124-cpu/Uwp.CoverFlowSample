5dc74ea9cacfd61f61681ba988284b0197cd7b53
macgo0124-cpu <macgo0124@gmail.com>
2026-06-02 23:01:35 -0700

    Actualizar GameSettings.cs

--- GameSettings.cs
+++ GameSettings.cs
@@ -4,10 +4,10 @@ public static class GameSettings
     public const bool IsEnabled = true;
 
     // Example settings for a custom game project
-    public const float CloseRangeAssist = .95f;
-    public const float MediumRangeAssist = 0.95f;
-    public const float LongRangeAssist = 0.95f;
-    public const float TrackingStrength = 0.95f;
+    public const float CloseRangeAssist = 0.10f;
+    public const float MediumRangeAssist = 0.25f;
+    public const float LongRangeAssist = 0.50f;
+    public const float TrackingStrength = 1.00f;
 }public static class GameSettings
 {
     public const bool IsEnabled = true;

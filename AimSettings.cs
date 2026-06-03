6bfe6e738be56d96b964fdae3e5083ed223815f1
macgo0124-cpu <macgo0124@gmail.com>
2026-06-02 22:46:45 -0700

    Actualizar AimSettings.cs

--- AimSettings.cs
+++ AimSettings.cs
@@ -5,14 +5,3 @@ public static class AimSettings
     public const float LongRangeAssist = 1.00f;
     public const float TrackingStrength = 1.00f;
     
-public class Weapon
-{
-
-    public int Damage = 500;
-
-    public float FireRate = 20.0f;
-
-    public int MagazineSize = 30;
-
-    public float ReloadTime = 2.0f;
-}

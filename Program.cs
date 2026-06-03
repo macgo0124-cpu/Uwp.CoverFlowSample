ef0b531e6c5c8f1d5b3d9d94d5d6e6c5ac62eb05
macgo0124-cpu <macgo0124@gmail.com>
2026-06-03 05:51:10 -0700

    Actualizar Program.cs

--- Program.cs
+++ Program.cs
@@ -100,7 +100,7 @@ namespace GearsStyleAI
             player.AimDirection = new Vector3(1, 0, 0);
 
             // AIM ASSIST
-            player.AimAssist(enemy, 9.00f);
+            player.AimAssist(enemy, 50.00f);
 
             // MEJOR COBERTURA
             CoverPoint bestCover =
